6552245b4692a87edb2995308241836a97d4c052
pengliang3676-afk <pengliang3676-afk@users.noreply.github.com>
2026-08-30 00:53:16 +0800

    Fix Mango TV bundle identifier

--- BDSpoofer.m
+++ BDSpoofer.m
@@ -3650,7 +3650,7 @@ static void bds_initialize() {
         loadConfig();
 
         NSString *bundleID = [NSBundle mainBundle].bundleIdentifier;
-        if (![bundleID isEqualToString:@"com.imgotv"]) return;
+        if (![bundleID isEqualToString:@"com.hunantv.imgotv"]) return;
 
         // 配置入口始终安装
         BDSInstallUI();
